e736a6ba28abf5cb5ab5cb57a6f9bf9c5a7b0a6b
Ketil Gjerde <raideer@gmail.com>
2023-09-14 18:07:35 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- main.tf
+++ main.tf
@@ -78,7 +78,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
 
     protected_settings = <<SETTINGS
     {
-        "fileUris": "https://raw.githubusercontent.com/mysteq/demo-terraform-vmss-ghrunner/script.sh",
+        "fileUris": [
+          "https://raw.githubusercontent.com/mysteq/demo-terraform-vmss-ghrunner/script.sh"
+          ],
         "commandToExecute": "sh script.sh ${var.github_org} ${var.github_key} label"
     }
     SETTINGS
