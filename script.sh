e76c5ba1574d1015325b875523fac03aa7540984
Ketil Gjerde <raideer@gmail.com>
2023-09-14 19:17:43 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- script.sh
+++ script.sh
@@ -14,5 +14,5 @@ mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.309.0.tar.gz
 ./config.sh --url $1 --token $2 --labels $3 --unattended
-./run.sh install
-./run.sh start
+./svc.sh install runasuser
+sudo ./svc.sh start
