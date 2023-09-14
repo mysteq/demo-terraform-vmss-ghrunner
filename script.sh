677fe39a4bb7f1f278f43fc028f73b87e3da3645
Ketil Gjerde <raideer@gmail.com>
2023-09-14 18:47:28 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ "`whoami`" != "runasuser" ]; then
+  useradd -m runasuser
+  sudo -u runasuser bash "$0" "$@"
+  exit
+fi
+
+cd ~
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.309.0.tar.gz
