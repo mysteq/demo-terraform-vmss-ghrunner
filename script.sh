a0d7681d89b91156e739463d2e7308a7789d45db
Ketil Gjerde <raideer@gmail.com>
2023-09-14 20:11:24 +0200

    chore: update
    Signed-off-by: Ketil Gjerde <raideer@gmail.com>

--- script.sh
+++ script.sh
@@ -16,4 +16,4 @@ cd ~
 mkdir actions-runner && cd actions-runner
 curl -o actions-runner-linux-x64-2.309.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.309.0/actions-runner-linux-x64-2.309.0.tar.gz
 tar xzf ./actions-runner-linux-x64-2.309.0.tar.gz
-./config.sh --url $1 --token $2 --labels $3 --unattended
+./config.sh --url $1 --token $2 --labels $3 --unattended --replace
